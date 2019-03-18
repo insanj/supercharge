ee692091319fed7eb26c0054aa5ca33954f05111
insanj <me@insanj.com>
2019-03-18 00:44:27 -0400

    add amazing new texture & anim, thunder/lightning sounds

--- plugin/src/main/java/com/insanj/supercharge/SuperchargeBlock.java
+++ plugin/src/main/java/com/insanj/supercharge/SuperchargeBlock.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 
-
 import net.minecraft.block.Block;
 import net.minecraft.block.RedstoneOreBlock;
 import net.minecraft.block.Material;
@@ -31,9 +30,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import com.google.common.collect.Multimap;
+
 public class SuperchargeBlock extends RedstoneOreBlock {
     public SuperchargeBlock() {
-      super(FabricBlockSettings.of(Material.STONE).hardness(0.2f).materialColor(MaterialColor.BLUE_TERRACOTTA).build()); // REDSTONE_LAMP ?
+      super(FabricBlockSettings.of(Material.STONE).hardness(0.5f).materialColor(MaterialColor.QUARTZ).build());
     }
 
     @Override
@@ -42,64 +43,36 @@ public class SuperchargeBlock extends RedstoneOreBlock {
 
       PlayerInventory playerInventory = playerEntity.inventory;
       ItemStack itemStack = playerInventory.getMainHandStack();
-      String displayName = "Supercharged!";
-      String attributeName = EntityAttributes.ATTACK_SPEED.getId();
-      EntityAttributeModifier.Operation operation = EntityAttributeModifier.Operation.values()[0];
-      EntityAttributeModifier modifier = new EntityAttributeModifier(displayName, 1000, operation);
-      EquipmentSlot slot = EquipmentSlot.HAND_MAIN;
-      itemStack.addAttributeModifier(attributeName, modifier, slot);
+      if (itemStack == null || itemStack.isEmpty()) {
+        playerEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, 0.1F, 1.0F);
+        return; // invalid item in main hand (or no item at all?)
+      }
 
-      playerEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, 1.0F, 1.0F);
+      EquipmentSlot slot = EquipmentSlot.HAND_MAIN;
+
+      Multimap existingModifiers = itemStack.getAttributeModifiers(slot);
+      String attributeName = "supercharged_attribute";
+      System.out.println(existingModifiers.keys().toString());
+      if (existingModifiers.containsKey(attributeName)) {
+        playerEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, 0.1F, 1.0F);
+        return; // already superchared this item!
+      }
+
+      EntityAttributeModifier.Operation operation = EntityAttributeModifier.Operation.values()[0];
+      EntityAttributeModifier modifier = new EntityAttributeModifier(attributeName, 1000, operation);
+      String attributeItemStackModifierName = EntityAttributes.ATTACK_SPEED.getId();
+      itemStack.addAttributeModifier(attributeItemStackModifierName, modifier, slot);
+
+      // done!
+      playerEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, 0.5F, 1.0F);
 
       return;
     }
-
-    public void onPlace
-
 /*
     @Override
-    public void spawnParticles(World world, BlockPos pos) {
-      particle.setColor(0, 1, 0);
-      return particle;
-    }
-*/
-/*
-  	public static Particle spawnParticle(EnumParticleTypes type, double par2, double par4, double par6, double par8, double par10, double par12)
-  	{
-      MinecraftClient mc = MinecraftClient.getInstance();
-  		if (mc != null && mc.getRenderViewEntity() != null && mc.effectRenderer != null)	{
-  			int var14 = mc.gameSettings.particleSetting;
+    public void onPlace(World world, BlockPos pos, BlockState state, PlayerEntity placer, ItemStack itemStack) {
+      super.onPlace(world, pos, state, placer, itemStack);
 
-  			if (var14 == 1 && mc.world.rand.nextInt(3) == 0)
-  			{
-  				var14 = 2;
-  			}
-
-  			double var15 = mc.getRenderViewEntity().posX - par2;
-  			double var17 = mc.getRenderViewEntity().posY - par4;
-  			double var19 = mc.getRenderViewEntity().posZ - par6;
-  			Particle var21 = null;
-  			double var22 = 16.0D;
-
-  			if (var15 * var15 + var17 * var17 + var19 * var19 > var22 * var22)
-  			{
-  				return null;
-  			}
-  			else if (var14 > 1)
-  			{
-  				return null;
-  			}
-  			else
-  			{
-  				if (type == EnumParticleTypes.EADORE_PORTAL)
-  				{
-  					var21 = new ParticleEadorePortal(mc.world, par2, par4, par6, par8, par10, par12);
-  				}
-
-  				mc.effectRenderer.addEffect(var21);
-  				return var21;
-  			}
-  		}
-  		return null;
-  	}*/
+      spawnParticles(world, pos);
+    }*/
 }
