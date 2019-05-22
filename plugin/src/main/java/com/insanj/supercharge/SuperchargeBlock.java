d9264f1001c6cad240710bb4577d9197a209961b
insanj <me@insanj.com>
2019-05-22 19:18:56 -0400

    fully localize strings, add item group, cleanups for 1.1.1

--- plugin/src/main/java/com/insanj/supercharge/SuperchargeBlock.java
+++ plugin/src/main/java/com/insanj/supercharge/SuperchargeBlock.java
@@ -43,81 +43,91 @@ import net.minecraft.world.BlockView;
 import com.google.common.collect.Multimap;
 
 public class SuperchargeBlock extends RedstoneOreBlock {
-    public SuperchargeBlock() {
-      super(FabricBlockSettings.of(Material.COBWEB).hardness(1.0f).lightLevel(15).build());
+  private final String SUPERCHARGE_TOOLTIP_FIRST_KEY = "tooltip.insanj_supercharge.supercharge_block.first_line";
+  private final String SUPERCHARGE_TOOLTIP_SECOND_KEY = "tooltip.insanj_supercharge.supercharge_block.second_line";
+
+  private final String SUPERCHARGE_ERROR_YOU_NEED_ITEM = "error.insanj_supercharge.you_need_item";
+  private final String SUPERCHARGE_ERROR_THING_CANNOT_WORK = "error.insanj_supercharge.thing_cannot_work";
+  private final String SUPERCHARGE_ERROR_ALREADY_SUPERCHARGED = "error.insanj_supercharge.already_supercharged";
+
+  private final String SUPERCHARGE_ATTRIBUTE_MOD_KEY = "Supercharge modifier";
+  private final String SUPERCHARGE_ITEM_PREFIX = "Supercharged ";
+
+  public SuperchargeBlock() {
+    super(FabricBlockSettings.of(Material.COBWEB).hardness(1.0f).lightLevel(15).build());
+  }
+
+  @Override
+  public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity playerEntity) {
+    super.onBreak(world, pos, state, playerEntity);
+
+    // step 1: get the inventory, and thus main hand item of the player who broke the block
+    PlayerInventory playerInventory = playerEntity.inventory;
+    ItemStack itemStack = playerInventory.getMainHandStack();
+    if (itemStack == null || itemStack.isEmpty()) {
+      sendErrorMessage(playerEntity, SUPERCHARGE_ERROR_YOU_NEED_ITEM);
+      world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.PLAYERS, 0.35F, 1.0F);
+      return; // invalid item in main hand (or no item at all?)
     }
 
-    @Override
-    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity playerEntity) {
-      super.onBreak(world, pos, state, playerEntity);
+    // step 2: setup the config for the supercharge -- In Main Hand, Attack Speed, x1000
+    final EquipmentSlot slot = EquipmentSlot.HAND_MAIN;
+    final String attributeItemStackModifierName = EntityAttributes.ATTACK_SPEED.getId();
+    final double attackSpeedModifierAmount = 1000.0;
 
-      // step 1: get the inventory, and thus main hand item of the player who broke the block
-      PlayerInventory playerInventory = playerEntity.inventory;
-      ItemStack itemStack = playerInventory.getMainHandStack();
-      if (itemStack == null || itemStack.isEmpty()) {
-        sendErrorMessage(playerEntity, "You need an item in your hand to Supercharge it!");
+    // step 3: grab the existing modifiers on the itemStack
+    // note: we need to ensure all these are still on the item at the end
+    Multimap existingModifiers = itemStack.getAttributeModifiers(slot);
+    if (existingModifiers.containsKey(attributeItemStackModifierName) == false) {
+      sendErrorMessage(playerEntity, SUPERCHARGE_ERROR_THING_CANNOT_WORK);
+      world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.PLAYERS, 0.35F, 1.0F);
+      return; // this thing should not be supercharged; it has no attack speed
+    }
+
+    Collection<EntityAttributeModifier> existingAttributes = (Collection<EntityAttributeModifier>)existingModifiers.get(attributeItemStackModifierName);
+
+    // step 4: create a new EntityAttributeModifier with the above specs
+    final EntityAttributeModifier.Operation operation = EntityAttributeModifier.Operation.values()[0];
+    final EntityAttributeModifier superchargedMod = new EntityAttributeModifier(SUPERCHARGE_ATTRIBUTE_MOD_KEY, attackSpeedModifierAmount, operation);
+
+    for (EntityAttributeModifier existingAttr : existingAttributes) {
+      if (existingAttr.getAmount() >= attackSpeedModifierAmount) {
+        sendErrorMessage(playerEntity, SUPERCHARGE_ERROR_ALREADY_SUPERCHARGED);
         world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.PLAYERS, 0.35F, 1.0F);
-        return; // invalid item in main hand (or no item at all?)
+        return; // already superchared this item!
       }
-
-      // step 2: setup the config for the supercharge -- In Main Hand, Attack Speed, x1000
-      final EquipmentSlot slot = EquipmentSlot.HAND_MAIN;
-      final String attributeItemStackModifierName = EntityAttributes.ATTACK_SPEED.getId();
-      final double attackSpeedModifierAmount = 1000.0;
-
-      // step 3: grab the existing modifiers on the itemStack
-      // note: we need to ensure all these are still on the item at the end
-      Multimap existingModifiers = itemStack.getAttributeModifiers(slot);
-      if (existingModifiers.containsKey(attributeItemStackModifierName) == false) {
-        sendErrorMessage(playerEntity, "This thing isn't meant to be Supercharged...");
-        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.PLAYERS, 0.35F, 1.0F);
-        return; // this thing should not be supercharged; it has no attack speed
-      }
-
-      Collection<EntityAttributeModifier> existingAttributes = (Collection<EntityAttributeModifier>)existingModifiers.get(attributeItemStackModifierName);
-
-      // step 4: create a new EntityAttributeModifier with the above specs
-      final EntityAttributeModifier.Operation operation = EntityAttributeModifier.Operation.values()[0];
-      final EntityAttributeModifier superchargedMod = new EntityAttributeModifier("Supercharge modifier", attackSpeedModifierAmount, operation);
-
-      for (EntityAttributeModifier existingAttr : existingAttributes) {
-        if (existingAttr.getAmount() >= attackSpeedModifierAmount) {
-          sendErrorMessage(playerEntity, "This item is already Supercharged!");
-          world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.PLAYERS, 0.35F, 1.0F);
-          return; // already superchared this item!
-        }
-      }
-
-      // step 5: add the supercharged modifier
-      itemStack.addAttributeModifier(attributeItemStackModifierName, superchargedMod, slot);
-
-      // step 6: add back the old modifiers which go missing for some reason
-      for (Object attributeName : existingModifiers.keySet()) {
-          Collection<EntityAttributeModifier> attributes = (Collection<EntityAttributeModifier>)existingModifiers.get(attributeName);
-          for (EntityAttributeModifier m : attributes) {
-            itemStack.addAttributeModifier((String)attributeName, m, slot);
-          }
-      }
-
-      // step 7: supercharge name
-      TranslatableTextComponent superchargedDisplayName = new TranslatableTextComponent("Supercharged " + itemStack.getDisplayName().getText());
-      itemStack.setDisplayName(superchargedDisplayName);
-
-      // step 8: refresh the player's inventory!
-      playerInventory.markDirty();
-
-      world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, SoundCategory.PLAYERS, 0.35F, 1.0F);
     }
 
-    public void sendErrorMessage(PlayerEntity player, String message) {
-      TranslatableTextComponent textComponent = new TranslatableTextComponent(message);
-      player.addChatMessage(textComponent, true);
+    // step 5: add the supercharged modifier
+    itemStack.addAttributeModifier(attributeItemStackModifierName, superchargedMod, slot);
+
+    // step 6: add back the old modifiers which go missing for some reason
+    for (Object attributeName : existingModifiers.keySet()) {
+      Collection<EntityAttributeModifier> attributes = (Collection<EntityAttributeModifier>)existingModifiers.get(attributeName);
+      for (EntityAttributeModifier m : attributes) {
+        itemStack.addAttributeModifier((String)attributeName, m, slot);
+      }
     }
 
-    @Override
-    public void buildTooltip(ItemStack stack, BlockView view, List<TextComponent> tooltip, TooltipContext options) {
-      tooltip.add(new TranslatableTextComponent("tooltip_one.insanj_supercharge.supercharge_block"));
-      tooltip.add(new TranslatableTextComponent("tooltip_two.insanj_supercharge.supercharge_block"));
-      super.buildTooltip(stack, view, tooltip, options);
-    }
+    // step 7: supercharge name
+    TranslatableTextComponent superchargedDisplayName = new TranslatableTextComponent(SUPERCHARGE_ITEM_PREFIX + itemStack.getDisplayName().getText());
+    itemStack.setDisplayName(superchargedDisplayName);
+
+    // step 8: refresh the player's inventory!
+    playerInventory.markDirty();
+
+    world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, SoundCategory.PLAYERS, 0.35F, 1.0F);
+  }
+
+  private void sendErrorMessage(PlayerEntity player, String message) {
+    TranslatableTextComponent textComponent = new TranslatableTextComponent(message);
+    player.addChatMessage(textComponent, true);
+  }
+
+  @Override
+  public void buildTooltip(ItemStack stack, BlockView view, List<TextComponent> tooltip, TooltipContext options) {
+    tooltip.add(new TranslatableTextComponent(SUPERCHARGE_TOOLTIP_FIRST_KEY));
+    tooltip.add(new TranslatableTextComponent(SUPERCHARGE_TOOLTIP_SECOND_KEY));
+    super.buildTooltip(stack, view, tooltip, options);
+  }
 }
--- plugin/src/main/java/com/insanj/supercharge/SuperchargeMod.java
+++ plugin/src/main/java/com/insanj/supercharge/SuperchargeMod.java
@@ -12,7 +12,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.CommandRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
-
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -31,24 +31,34 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 
 public class SuperchargeMod implements ModInitializer {
-    public static final String MOD_ID = "insanj_supercharge";
-    public static final String BLOCK_ID = "supercharge_block";
-    public static final Block SUPERCHARGE_BLOCK = new SuperchargeBlock();
-    public static final BlockItem SUPERCHARGE_BLOCK_ITEM =  new BlockItem(SUPERCHARGE_BLOCK, new Item.Settings().itemGroup(ItemGroup.MISC));
+  private static final String MOD_ID = "insanj_supercharge";
+  private static final String BLOCK_ID = "supercharge_block";
+  private static final String SUPERCHARGE_ITEM_GROUP_NAME = "supercharge";
+  private static final String SUPERCHARGE_COMMAND = "supercharge";
 
-    @Override
-    public void onInitialize() {
-      Registry.register(Registry.BLOCK, new Identifier(MOD_ID, BLOCK_ID), SUPERCHARGE_BLOCK);
-      Registry.register(Registry.ITEM, new Identifier(MOD_ID, BLOCK_ID), SUPERCHARGE_BLOCK_ITEM);
+  private static final Block SUPERCHARGE_BLOCK = new SuperchargeBlock();
+  private static final BlockItem SUPERCHARGE_BLOCK_ITEM =  new BlockItem(SUPERCHARGE_BLOCK, new Item.Settings().itemGroup(ItemGroup.MISC));
+  private static final ItemGroup SUPERCHARGE_ITEM_GROUP = FabricItemGroupBuilder.create(
+      new Identifier(MOD_ID, SUPERCHARGE_ITEM_GROUP_NAME))
+      .icon(() -> new ItemStack(SUPERCHARGE_BLOCK_ITEM))
+      .appendItems(stacks ->{
+        stacks.add(new ItemStack(SUPERCHARGE_BLOCK_ITEM));
+      })
+    .build();
 
-      CommandRegistry.INSTANCE.register(false, serverCommandSourceCommandDispatcher -> serverCommandSourceCommandDispatcher.register(
-        CommandManager.literal("supercharge").requires(source -> source.hasPermissionLevel(4))
-          .executes(context -> {
-            ServerPlayerEntity senderPlayer = context.getSource().getPlayer();
-            senderPlayer.inventory.insertStack(new ItemStack(SUPERCHARGE_BLOCK_ITEM));
-            senderPlayer.inventory.markDirty();
-            return 1;
-          })
-      ));
-    }
+  @Override
+  public void onInitialize() {
+    Registry.register(Registry.BLOCK, new Identifier(MOD_ID, BLOCK_ID), SUPERCHARGE_BLOCK);
+    Registry.register(Registry.ITEM, new Identifier(MOD_ID, BLOCK_ID), SUPERCHARGE_BLOCK_ITEM);
+
+    CommandRegistry.INSTANCE.register(false, serverCommandSourceCommandDispatcher -> serverCommandSourceCommandDispatcher.register(
+      CommandManager.literal(SuperchargeMod.SUPERCHARGE_COMMAND).requires(source -> source.hasPermissionLevel(4))
+        .executes(context -> {
+          ServerPlayerEntity senderPlayer = context.getSource().getPlayer();
+          senderPlayer.inventory.insertStack(new ItemStack(SUPERCHARGE_BLOCK_ITEM));
+          senderPlayer.inventory.markDirty();
+          return 1;
+        })
+    ));
+  }
 }
